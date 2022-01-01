dd4eb77daa619f32340e204ec1e2bc36b3b5e0d0
RAVI SHARMA <ravics09@RAVIs-MacBook-Air.local>
2022-01-01 14:40:31 +0530

    Created maidDetail screen

---
+++ src/screens/MaidDetail/maidDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Button} from 'react-native';
+
+const MaidDetail = () => {
+    return(
+        <View>
+            <Text>This is MaidDetail screen</Text>
+        </View>
+    )
+}
+
+export default MaidDetail;
